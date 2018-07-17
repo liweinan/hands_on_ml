75fcd4c43e18f74661f73ee90f03b3d461a1134a
Weinan Li <l.weinan@gmail.com>
2018-07-17 18:09:38 +0800

    .

--- operations.py
+++ operations.py
@@ -8,18 +8,18 @@ def roundup(x):
 
 list = []
 
-for i in range(5):
+for i in range(8):
     x = random.randint(5, 10)
     y = random.randint(5, 10)
     list.append("{0} + {1} =".format(x, y))
 
-for i in range(5):
+for i in range(8):
     x = random.randint(10, 50)
     y = random.randint(10, 50)
     list.append("{0} + {1} =".format(x, y))
 
 # 千位和百位的加减法3道，百位需要是整数
-for i in range(1):
+for i in range(2):
     x = roundup(random.randint(100, 499))
     y = roundup(random.randint(100, 499))
     list.append("{0} + {1} =".format(x, y))
@@ -38,32 +38,32 @@ for i in range(2):
     list.append("{0} ÷ {1} =".format(z, y))
 
 
-for i in range(10):
-    x = random.randint(1, 9)
-    y = random.randint(1, 9)
+for i in range(18):
+    x = random.randint(10, 99)
+    y = random.randint(10, 50)
     if x < y:
         x, y = y, x
     list.append("{0} - {1} =".format(x, y))
 
-for i in range(10):
+for i in range(20):
     x = random.randint(1, 9)
     y = random.randint(1, 9)
     z = x * y
     list.append("{0} ÷ {1} =".format(z, y))
 
-for i in range(10):
+for i in range(20):
     x = random.randint(1, 9)
     y = random.randint(1, 9)
     list.append("{0} × {1} =".format(x, y))
 
-for i in range(2):
+for i in range(7):
     x = random.randint(1, 4)
     y = random.randint(1, 5)
     z = random.randint(1, 9)
     r = (x + y) * z
     list.append("({0} + {1}) × {2} =".format(x, y, z))
 
-for i in range(2):
+for i in range(7):
     x = random.randint(1, 9)
     y = random.randint(1, 9)
     if x == y:
@@ -74,7 +74,7 @@ for i in range(2):
     r = (x - y) * z
     list.append("{0} ÷ ({1} - {2}) =".format(r, x, y))
 
-for i in range(1):
+for i in range(6):
     x = random.randint(1, 2)
     y = random.randint(1, 3)
     z = random.randint(1, 3)
